6032db54b075f12c56bf191dc8c2827bfc9e1a3f
Cenk Altı <cenkalti@gmail.com>
2024-12-18 14:02:42 +0300

    add comments

--- error.go
+++ error.go
@@ -20,22 +20,27 @@ func Permanent(err error) error {
 	}
 }
 
+// Error returns a string representation of the Permanent error.
 func (e *PermanentError) Error() string {
 	return e.Err.Error()
 }
 
+// Unwrap returns the wrapped error.
 func (e *PermanentError) Unwrap() error {
 	return e.Err
 }
 
+// RetryAfterError signals that the operation should be retried after the given duration.
 type RetryAfterError struct {
 	Duration time.Duration
 }
 
+// RetryAfter returns a RetryAfter error that specifies how long to wait before retrying.
 func RetryAfter(seconds int) error {
 	return &RetryAfterError{Duration: time.Duration(seconds) * time.Second}
 }
 
+// Error returns a string representation of the RetryAfter error.
 func (e *RetryAfterError) Error() string {
 	return fmt.Sprintf("retry after %s", e.Duration)
 }
